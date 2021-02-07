7e3dfa5ad9a923b45a4107c665fff696969d4eb7
Dehvon Curtis <dcurtis@macbook.local>
2021-02-07 15:15:46 -0700

    ticker sorting functional, writes stocks.txt ready for feeding

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ stock market analysis tool for anomaly and volume detection
 
 ## Setup
 1) Download ticker csv https://www.nasdaq.com/market-activity/stocks/screener?exchange=nyse&letter=0&render=download
-2) Add ticker file to var
+2) Add filename to script
 3) Run ticker-prep.py script to strip tickers
 4) 
--- ticker-prep.py
+++ ticker-prep.py
@@ -1,6 +1,21 @@
-import json
+import pandas
 
-ticker_file = '' # add ticker csv location
+# add filename downloaded from nasdaq
+file = 'nasdaq_screener_1612650590475.csv'
 
-with open('results_2021-02-06.json', 'r') as file:
-    f
+csv_file = pandas.read_csv(file)
+
+symbol_column = csv_file.Symbol
+
+symbol_list = []
+
+# removes tickers with multiple classes as noted with '^' in nasdaq list (example: ABR^A)
+for ticker in symbol_column:
+    symbol_list.append(ticker)
+    if '^' in ticker:
+        symbol_list.pop()
+
+# write tickers to new file
+with open('stocks.txt', 'w') as ticker_file:
+    for ticker in symbol_list:
+        ticker_file.write(f'{ticker}\n')
